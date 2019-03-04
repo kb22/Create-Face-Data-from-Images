402830c6c03df1711c2341b0345747a40fc007ae
Bhanot, Karan <k.bhanot@cvent.com>
2019-03-04 13:28:23 +0530

    Add check for image file extensions

--- data_generator.py
+++ data_generator.py
@@ -18,24 +18,28 @@ if not os.path.exists('updated_images'):
 
 # Loop through all images and save images with marked faces
 for file in os.listdir(base_dir + 'images'):
-	image = cv2.imread(base_dir + 'images/' + file)
+	file_name, file_extension = os.path.splitext(file)
+	if (file_extension in ['.png','.jpg']):
+		print("Image path: {}".format(base_dir + 'images/' + file))
 
-	(h, w) = image.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+		image = cv2.imread(base_dir + 'images/' + file)
 
-	model.setInput(blob)
-	detections = model.forward()
+		(h, w) = image.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
-	# Create frame around face
-	for i in range(0, detections.shape[2]):
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
+		model.setInput(blob)
+		detections = model.forward()
 
-		confidence = detections[0, 0, i, 2]
+		# Create frame around face
+		for i in range(0, detections.shape[2]):
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
 
-		# If confidence > 0.5, show box around face
-		if (confidence > 0.5):
-			cv2.rectangle(image, (startX, startY), (endX, endY), (255, 255, 255), 2)
+			confidence = detections[0, 0, i, 2]
 
-	cv2.imwrite(base_dir + 'updated_images/' + file, image)
-	print("Image " + file + " converted successfully")
+			# If confidence > 0.5, show box around face
+			if (confidence > 0.5):
+				cv2.rectangle(image, (startX, startY), (endX, endY), (255, 255, 255), 2)
+
+		cv2.imwrite(base_dir + 'updated_images/' + file, image)
+		print("Image " + file + " converted successfully")
--- face_extractor.py
+++ face_extractor.py
@@ -19,25 +19,27 @@ if not os.path.exists('faces'):
 # Loop through all images and strip out faces
 count = 0
 for file in os.listdir(base_dir + 'images'):
-	image = cv2.imread(base_dir + 'images/' + file)
+	file_name, file_extension = os.path.splitext(file)
+	if (file_extension in ['.png','.jpg']):
+		image = cv2.imread(base_dir + 'images/' + file)
 
-	(h, w) = image.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+		(h, w) = image.shape[:2]
+		blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
-	model.setInput(blob)
-	detections = model.forward()
+		model.setInput(blob)
+		detections = model.forward()
 
-	# Create frame around face
-	for i in range(0, detections.shape[2]):
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
+		# Identify each face
+		for i in range(0, detections.shape[2]):
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
 
-		confidence = detections[0, 0, i, 2]
+			confidence = detections[0, 0, i, 2]
 
-		# If confidence > 0.5, show box around face
-		if (confidence > 0.5):
-			count += 1
-			frame = image[startY:endY, startX:endX]
-			cv2.imwrite(base_dir + 'faces/' + str(i) + '_' + file, frame)
+			# If confidence > 0.5, save it as a separate file
+			if (confidence > 0.5):
+				count += 1
+				frame = image[startY:endY, startX:endX]
+				cv2.imwrite(base_dir + 'faces/' + str(i) + '_' + file, frame)
 
 print("Extracted " + str(count) + " faces from all images")
